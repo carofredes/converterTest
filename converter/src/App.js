3287265134becd81ff825fc83575af52b218328e
Carolina Fredes <carofre13@gmail.com>
2021-04-16 20:51:39 -0300

    Adding initial structure for components

--- converter/src/App.js
+++ converter/src/App.js
@@ -1,7 +1,16 @@
 import './App.css';
+import Footer from './Components/Footer';
+import Header from './Components/Header';
+import TabContainer from './Components/TabContainer/TabContainer';
 
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<Header />
+			<TabContainer />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
---
+++ converter/src/Components/TabContainer/TabContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Converter from './Converter';
+
+export default class TabContainer extends Component {
+	render() {
+		return (
+			<div>
+				TabContainer
+				<Converter />
+			</div>
+		);
+	}
+}
