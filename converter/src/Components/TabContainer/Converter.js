fb54c0e5f3ed254c5d85571bcab8495c6e14d47a
Carolina Fredes <carofre13@gmail.com>
2021-04-16 23:30:56 -0300

    Updating converter with initial form

--- converter/src/Components/TabContainer/Converter.js
+++ converter/src/Components/TabContainer/Converter.js
@@ -1,7 +1,49 @@
 import React, { Component } from 'react';
+import { MDBContainer, MDBRow, MDBCol, MDBBtn, MDBIcon } from 'mdbreact';
 
 export default class Converter extends Component {
 	render() {
-		return <div>Converter</div>;
+		return (
+			<MDBContainer>
+				<form>
+					<MDBRow>
+						<MDBCol md='6'>
+							<MDBRow>
+								<MDBCol md='12'>
+									<label htmlFor='fromField' className='grey-text font-weight-light'>
+										From
+									</label>
+									<input type='text' id='fromField' className='form-control' />
+								</MDBCol>
+								<MDBCol md='12'>
+									<label htmlFor='ammountFromField' className='grey-text font-weight-light'>
+										Ammount:
+									</label>
+									<input type='text' id='ammountFromField' className='form-control' />
+								</MDBCol>
+							</MDBRow>
+						</MDBCol>
+						<MDBCol md='6'>
+							<MDBRow>
+								<MDBCol md='12'>
+									<label htmlFor='toField' className='grey-text font-weight-light'>
+										To
+									</label>
+									<input type='email' id='toField' className='form-control' />
+								</MDBCol>
+
+								<MDBCol md='12'>
+									<label htmlFor='ammountToField' className='grey-text font-weight-light'>
+										Ammount:
+									</label>
+									<input type='email' id='ammountToField' className='form-control' />
+								</MDBCol>
+							</MDBRow>
+						</MDBCol>
+					</MDBRow>
+					<MDBBtn className='btn'>Convert</MDBBtn>
+				</form>
+			</MDBContainer>
+		);
 	}
 }
--- converter/src/Components/TabContainer/TabContainer.js
+++ converter/src/Components/TabContainer/TabContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { MDBContainer, MDBTabPane, MDBTabContent, MDBNav, MDBNavItem, MDBNavLink } from 'mdbreact';
+import { MDBContainer, MDBTabPane, MDBTabContent, MDBNav, MDBNavItem, MDBNavLink, MDBIcon } from 'mdbreact';
 import Converter from './Converter';
 
 class TabContainer extends Component {
@@ -25,18 +25,19 @@ class TabContainer extends Component {
 				<MDBNav className='nav-tabs mt-5'>
 					<MDBNavItem>
 						<MDBNavLink link to='#' active={activeItem === '1'} onClick={this.toggle('1')} role='tab'>
-							Converter
+							<MDBIcon icon='coins' className='mr-2' /> Converter
 						</MDBNavLink>
 					</MDBNavItem>
 					<MDBNavItem>
 						<MDBNavLink link to='#' active={activeItem === '2'} onClick={this.toggle('2')} role='tab'>
+							<MDBIcon far icon='chart-bar' className='mr-2' />
 							Charts
 						</MDBNavLink>
 					</MDBNavItem>
 				</MDBNav>
 				<MDBTabContent activeItem={activeItem}>
 					<MDBTabPane tabId='1' role='tabpanel'>
-						<p className='mt-2'>Converter</p>
+						<Converter />
 					</MDBTabPane>
 					<MDBTabPane tabId='2' role='tabpanel'>
 						<p className='mt-2'>Charts</p>
