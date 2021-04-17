41237cf78c128e10f4a473d1a8017dfaca4b38d4
Carolina Fredes <carofre13@gmail.com>
2021-04-16 21:42:23 -0300

    Adding MDBreact for styles and adding tabs to TabContainer component

--- converter/src/App.js
+++ converter/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import TabContainer from './Components/TabContainer/TabContainer';
 
 function App() {
 	return (
-		<div className='App'>
-			<Header />
-			<TabContainer />
-			<Footer />
-		</div>
+		<BrowserRouter>
+			<div className='App'>
+				<Header />
+				<TabContainer />
+				<Footer />
+			</div>
+		</BrowserRouter>
 	);
 }
 
--- converter/src/Components/TabContainer/TabContainer.js
+++ converter/src/Components/TabContainer/TabContainer.js
@@ -1,13 +1,49 @@
 import React, { Component } from 'react';
+import { MDBContainer, MDBTabPane, MDBTabContent, MDBNav, MDBNavItem, MDBNavLink } from 'mdbreact';
 import Converter from './Converter';
 
-export default class TabContainer extends Component {
+class TabContainer extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			activeItem: '1',
+		};
+	}
+
+	toggle = (tab) => (e) => {
+		if (this.state.activeItem !== tab) {
+			this.setState({
+				activeItem: tab,
+			});
+		}
+	};
+
 	render() {
+		const { activeItem } = this.state;
 		return (
-			<div>
-				TabContainer
-				<Converter />
-			</div>
+			<MDBContainer>
+				<MDBNav className='nav-tabs mt-5'>
+					<MDBNavItem>
+						<MDBNavLink link to='#' active={activeItem === '1'} onClick={this.toggle('1')} role='tab'>
+							Converter
+						</MDBNavLink>
+					</MDBNavItem>
+					<MDBNavItem>
+						<MDBNavLink link to='#' active={activeItem === '2'} onClick={this.toggle('2')} role='tab'>
+							Charts
+						</MDBNavLink>
+					</MDBNavItem>
+				</MDBNav>
+				<MDBTabContent activeItem={activeItem}>
+					<MDBTabPane tabId='1' role='tabpanel'>
+						<p className='mt-2'>Converter</p>
+					</MDBTabPane>
+					<MDBTabPane tabId='2' role='tabpanel'>
+						<p className='mt-2'>Charts</p>
+					</MDBTabPane>
+				</MDBTabContent>
+			</MDBContainer>
 		);
 	}
 }
+export default TabContainer;
